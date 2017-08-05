f1e8307ef7d677515355c7a21b7d1e658cca6890
Kirill <kirillzzy@gmail.com>
2017-08-05 09:04:36 +0300

    Create Xcode project

---
+++ lksh-ios-cream/ViewControllers/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  lksh-ios-cream
+//
+//  Created by Kirill Averyanov on 05/08/2017.
+//  Copyright © 2017 Kirill Averyanov. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+}
+
